0a9c50fa00da4ecc6b779b14d65d69cc01e47286
Jaschar Domann <domann@tiba-tech.de>
2021-02-14 13:35:44 +0100

    rearrange test folder structure

---
+++ test/test_state.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class StateTestCase(unittest.TestCase):
+
+    def test_something(self):
+        self.assertEqual(True, True)
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
