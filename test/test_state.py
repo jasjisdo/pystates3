90c28a4f465cbbbb156b77c41d54a0efe4a2ce8a
Jaschar Domann <domann@tiba-tech.de>
2021-02-14 14:30:25 +0100

    tests transition behavior.

--- test/test_state.py
+++ test/test_state.py
@@ -1,10 +1,33 @@
 import unittest
 
+from state import (
+    State,
+    Predicate,
+    Transition,
+    Context
+)
+
 
 class StateTestCase(unittest.TestCase):
 
-    def test_something(self):
-        self.assertEqual(True, True)
+    def test_state_init(self):
+        state = State("TestState")
+        self.assertEqual(state.get_name(), "TestState")
+        pass
+
+    def test_state_transition(self):
+        end_state = State("EndState")
+        transition = Transition(
+            Predicate(lambda: True),
+            lambda: print("Hello Reflex"),
+            end_state
+        )
+        start_state = State("StartState", transition)
+        context = Context()
+        context.set_current_state(start_state)
+        start_state.do_transition(context)
+        self.assertEqual(context.get_current_state(), end_state)
+        self.assertEqual(context.get_previous_state(), start_state)
         pass
 
 
